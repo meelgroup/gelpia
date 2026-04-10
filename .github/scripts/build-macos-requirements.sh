bab16e7816a3c7fcf54aba31ef03fc5607a65c7c
Uddalok <uddaloksarkar@gmail.com>
2026-04-10 19:23:13 -0400

    updated CI

--- .github/scripts/build-macos-requirements.sh
+++ .github/scripts/build-macos-requirements.sh
@@ -15,8 +15,8 @@ export PATH="$BREW_PREFIX/opt/bison/bin:$BREW_PREFIX/opt/flex/bin:$PATH"
 export LIBRARY_PATH="$REQ_DIR/lib:${LIBRARY_PATH:-}"
 export C_INCLUDE_PATH="$REQ_DIR/include:${C_INCLUDE_PATH:-}"
 export CPLUS_INCLUDE_PATH="$REQ_DIR/include:${CPLUS_INCLUDE_PATH:-}"
-export CPPFLAGS="-I$BREW_PREFIX/opt/bison/include -I$BREW_PREFIX/opt/flex/include ${CPPFLAGS:-}"
-export LDFLAGS="-L$BREW_PREFIX/opt/bison/lib -L$BREW_PREFIX/opt/flex/lib ${LDFLAGS:-}"
+export CPPFLAGS="-I$REQ_DIR/include -I$BREW_PREFIX/opt/bison/include -I$BREW_PREFIX/opt/flex/include ${CPPFLAGS:-}"
+export LDFLAGS="-L$REQ_DIR/lib -L$BREW_PREFIX/opt/bison/lib -L$BREW_PREFIX/opt/flex/lib ${LDFLAGS:-}"
 export CFLAGS="-fPIC ${CFLAGS:-}"
 export CXXFLAGS="-std=c++11 -fPIC ${CXXFLAGS:-}"
 
@@ -31,25 +31,30 @@ make
 make install
 
 cd "$BUILD_DIR"
-curl -L https://downloads.sourceforge.net/project/gaol/gaol/4.2.0/gaol-4.2.0.tar.gz -o gaol.tar.gz
-tar -xf gaol.tar.gz
-patch -p0 < "$ROOT_DIR/documents/gaol-4.2.0.patch"
-mv gaol-4.2.0 gaol
-cd gaol
-
-gaol_config_flags=(
-  --with-mathlib=crlibm
-  --disable-debug
-  --disable-preserve-rounding
-  --enable-optimize
-  --disable-verbose-mode
-  --prefix="$REQ_DIR"
-)
-
 if [ "$(uname -m)" = "x86_64" ]; then
-  gaol_config_flags+=(--enable-simd)
-fi
+  curl -L https://downloads.sourceforge.net/project/gaol/gaol/4.2.0/gaol-4.2.0.tar.gz -o gaol.tar.gz
+  tar -xf gaol.tar.gz
+  patch -p0 < "$ROOT_DIR/documents/gaol-4.2.0.patch"
+  mv gaol-4.2.0 gaol
+  cd gaol
 
-./configure "${gaol_config_flags[@]}"
-make
-make install
+  ./configure \
+    --with-mathlib=crlibm \
+    --disable-debug \
+    --disable-preserve-rounding \
+    --enable-optimize \
+    --disable-verbose-mode \
+    --enable-simd \
+    --prefix="$REQ_DIR"
+  make
+  make install
+else
+  git clone https://github.com/goualard-f/GAOL.git gaol
+  cd gaol
+  git checkout cd0ee1a75febab97a7f6c18a03e31780a2717f2c
+  patch -p1 < "$ROOT_DIR/documents/gaol-4.2.0.patch"
+
+  meson setup build --prefix="$REQ_DIR" -Dwith-mathlib=crlibm
+  meson compile -C build
+  meson install -C build
+fi
